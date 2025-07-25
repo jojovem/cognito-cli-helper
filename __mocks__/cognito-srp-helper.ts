16b2b9d7f152b5b4b1f1daf94125fa83d54c9640
jojovem <jojovembh@gmail.com>
2025-07-25 02:00:58 -0300

    test: add testing mocks

---
+++ __mocks__/cognito-srp-helper.ts
@@ -0,0 +1,4 @@
+export const createSrpSession   = () => ({});
+export const signSrpSession     = () => ({});
+export const wrapAuthChallenge  = (_sig: unknown, params: unknown) => params;
+export const wrapInitiateAuth   = (_sess: unknown, params: unknown) => params;
